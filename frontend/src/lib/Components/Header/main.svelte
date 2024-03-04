3cff4abbcbe9d430167be38edb3dfd1278a6b00e
div-styl <gouhmada@gmail.com>
2024-03-04 22:17:07 +0100

    Refactor header component layout

--- frontend/src/lib/Components/Header/main.svelte
+++ frontend/src/lib/Components/Header/main.svelte
@@ -3,12 +3,9 @@
 	import Desktop from './desktop.svelte';
 </script>
 
-<header class="w-full m-auto text-2xl md:text-5xl border-b-[0.5px] border-gray-300">
-	<div class="md:hidden">
-			<Mobile />
-	</div>
-	<div class="hidden md:block">
-		<Desktop />
-	</div>
-</header>
-
+<div class="md:hidden">
+	<Mobile />
+</div>
+<div class="hidden md:block">
+	<Desktop />
+</div>
--- frontend/src/lib/Components/Header/mobile.svelte
+++ frontend/src/lib/Components/Header/mobile.svelte
@@ -1,7 +1,30 @@
 <script>
-  let msg = "from mobile"
+	let offerAid = 'Offers of Aid';
+	const category = ['Food', 'Clothes', 'Shelter', 'Health', 'Adoption'];
+	const filter = () => {
+		category.forEach((cat) => {
+			console.log(cat);
+		});
+	};
 </script>
 
-	<div class="p-5 flex text-center justify-center">
-		{msg}
+<header>
+	<!-- TODO: Avatar -->
+	<!-- !Head title -->
+	<div class="p-2 flex text-center justify-center font-semibold text-lg">
+		{offerAid}
 	</div>
+	<!-- TODO: filter design -->
+	<div class="flex flex-row pt-5 overflow-x-scroll">
+		{#each category as cat}
+			<div
+				class="flex flex-auto justify-evenly  border rounded-lg border-gray-300 bg-gray-200/50 mx-3 dark:border-gray-600/60 dark:text-white"
+			>
+				<button on:click={filter} class=" px-2 py-1">
+					<input type="radio" class="hidden" id={cat} />
+					<label class="cursor-pointer" for={cat}>{cat}</label>
+				</button>
+			</div>
+		{/each}
+	</div>
+</header>
