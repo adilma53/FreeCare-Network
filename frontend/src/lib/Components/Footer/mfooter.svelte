38176412f7c177e376591bbf6d26c0b7e5c5e4a5
div-styl <gouhmada@gmail.com>
2024-03-07 00:59:57 +0100

    footer is here :rocket:

--- frontend/src/lib/Components/Footer/mfooter.svelte
+++ frontend/src/lib/Components/Footer/mfooter.svelte
@@ -2,15 +2,15 @@
 	import { goto } from '$app/navigation';
 	import Icon from '@iconify/svelte';
 	const home = async () => {
-		goto('/')
+		goto('/');
 	};
-	
+
 	const post = async () => {
-		goto('/post')
+		goto('/post');
 	};
 
 	const login = async () => {
-		goto('/usr/login')
+		goto('/usr/login');
 	};
 </script>
 
@@ -18,13 +18,13 @@
 	class="fixed bottom-5 rounded-xl z-50 w-full h-16 bg-surface-200 dark:bg-surface-600 dark:border-[0.5px] border-surface-700"
 >
 	<div class="grid h-full flex items-stratch grid-cols-3 mx-auto">
-		<button on:click="{home}">
+		<button on:click={home}>
 			<Icon icon="mingcute:home-3-fill" class="dark:text-surface-200 ml-10 h-8 w-8" />
 		</button>
-		<button on:click="{post}" type="button">
+		<button on:click={post} type="button">
 			<Icon icon="material-symbols-light:add" class="dark:text-surface-200 ml-10 h-8 w-8" />
 		</button>
-		<button on:click="{login}" type="button">
+		<button on:click={login} type="button">
 			<Icon icon="iconamoon:profile-thin" class="dark:text-surface-200 ml-10 h-8 w-8" />
 		</button>
 	</div>
