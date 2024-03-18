db3f1281444d3ac59713ebd79360cb55c02e6a60
div-styl <gouhmada@gmail.com>
2024-03-18 23:29:03 +0000

    issue with the aside bar

---
+++ frontend/src/lib/Components/Body/body_desktop.svelte
@@ -0,0 +1,10 @@
+<script>
+	let Home = 'Home';
+</script>
+
+<header
+	class="border-b border-surface-600 dark:border-b dark:border-surface-300 ">
+	<h1 class="text-3xl font-bold flex items-center justify-right">
+		{Home}
+	</h1>
+</header>
