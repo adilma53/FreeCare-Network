0307b591baa376c20ac48bf2993edf173f85ea2c
div-styl <gouhmada@gmail.com>
2024-03-18 12:39:39 +0000

    rebuild the bottom nav for mobile users more enhanced in desing matter

--- frontend/src/lib/Components/Footer/mfooter.svelte
+++ frontend/src/lib/Components/Footer/mfooter.svelte
@@ -15,17 +15,59 @@
 </script>
 
 <div
-	class="fixed bottom-5 rounded-xl z-50 w-full h-16 border-[0.5px] border-surface-100 dark:border-none dark:bg-surface-600 dark:border-[0.5px] border-surface-700"
->
-	<div class="grid h-full flex items-stratch grid-cols-3 mx-auto">
-		<button on:click={home}>
-			<Icon icon="mingcute:home-3-fill" class="dark:text-surface-200 ml-10 h-8 w-8" />
+	class="fixed bottom-0 left-0 z-50 w-full h-16 bg-surface-50 border-t border-gray-400 dark:bg-surface-700 dark:border-gray-600">
+	<div class="grid h-full max-w-lg grid-cols-3 mx-auto font-medium">
+		<button
+			on:click={home}
+			class="inline-flex flex-col items-center justify-center px-5 hover:bg-surface-100 dark:hover:bg-surface-600 group">
+			<svg
+				class="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="currentColor"
+				viewBox="0 0 20 20">
+				<path
+					d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+			</svg>
+			<span
+				class="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+				>Home</span>
 		</button>
-		<button on:click={post} type="button">
-			<Icon icon="material-symbols-light:add" class="dark:text-surface-200 ml-10 h-8 w-8" />
+		<button
+			on:click={post}
+			class="inline-flex flex-col items-center justify-center px-5 hover:bg-surface-100 dark:hover:bg-surface-600 group">
+			<svg
+				class="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				width="1em"
+				height="1em"
+				viewBox="-0.5 -0.5 24 24"
+				{...$$props}>
+				<path
+					fill="currentColor"
+					d="m21.289.98l.59.59c.813.814.69 2.257-.277 3.223L9.435 16.96l-3.942 1.442c-.495.182-.977-.054-1.075-.525a.928.928 0 0 1 .045-.51l1.47-3.976L18.066 1.257c.967-.966 2.41-1.09 3.223-.276zM8.904 2.19a1 1 0 1 1 0 2h-4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-4a1 1 0 0 1 2 0v4a4 4 0 0 1-4 4h-12a4 4 0 0 1-4-4v-12a4 4 0 0 1 4-4z" />
+			</svg>
+			<span
+				class="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+				>Post</span>
 		</button>
-		<button on:click={login} type="button">
-			<Icon icon="iconamoon:profile-thin" class="dark:text-surface-200 ml-10  h-8 w-8" />
+		<button
+			on:click={login}
+			class="inline-flex flex-col items-center justify-center px-5 hover:bg-surface-100 dark:hover:bg-surface-600 group">
+			<svg
+				class="w-5 h-5 mb-2 text-gray-500 dark:text-surface-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="currentColor"
+				viewBox="0 0 20 20">
+				<path
+					d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+			</svg>
+			<span
+				class="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+				>Profile</span>
 		</button>
 	</div>
 </div>
+
--- frontend/src/lib/Components/Forms/posts/main.svelte
+++ frontend/src/lib/Components/Forms/posts/main.svelte
@@ -1,9 +1,11 @@
 <script>
 	import PostMobile from './post_mobile.svelte';
-	// import LoginDesktop from './login_mobile.svelte';
+	import PostDesktop from './post_desktop.svelte';
 </script>
 
 <div class="md:hidden">
 	<PostMobile />
 </div>
-<div class="hidden md:block"></div>
+<div class="hidden md:block">
+	<PostDesktop />
+</div>
