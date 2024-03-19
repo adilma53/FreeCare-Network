88b8533ef9a480fe1fca4c111a0cf31e8656ed00
adil majhoul <itsadilhadid.com>
2024-03-19 16:53:49 +0000

    working on post finished design

--- frontend/src/lib/Components/Body/body_mobile.svelte
+++ frontend/src/lib/Components/Body/body_mobile.svelte
@@ -1,38 +1,53 @@
 <script>
 	import Food from '$lib/images/food.jpg';
-	const claim = () => {
-		console.log('claim');
-	};
+	import { Avatar, Badge, Progressbar } from 'flowbite-svelte';
+	import { ClockSolid } from 'flowbite-svelte-icons';
+
+	let progress;
+
+	let imgsrc =
+		'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 </script>
 
 <section>
 	<!-- container -->
-	<div class="container px-5 py-9 mx-auto">
+	<div class=" container px-5 mx-auto">
 		<!-- post card start here -->
-		<div class="grid grid-cols-1 gap-8">
-			<div>
-				<img class="object-cover object-center w-full h-64 rounded-lg" src={Food} alt="" />
-
-				<div class="mt-5">
-					<h1 class="mt-4 text-xl font-bold text-gray-800 dark:text-white">Free meals</h1>
-
-					<p class="mt-2 text-gray-500 dark:text-gray-400">
-						I have 10 meal vouchers for a local restaurant. DM me if you need it.
-					</p>
-					<div
-						class="flex justify-between pt-2
-						"
-					>
-						<p class="text-lg pt-2 font-meduim text-gray-500">3 hourse ago</p>
-						<button
-							on:click={claim}
-							class="p-2 px-3 rounded-xl bg-primary-400 text-white font-semibold"
-						>
-							Claim Offer
-						</button>
-					</div>
-				</div>
+		<!-- post owner name and post date -->
+		<div class="px-1 flex my-3 items-center">
+			<img class="rounded-full size-12 ring ring-blue-500 dark:ring-blue-300" src={imgsrc} />
+			<div class=" mx-3">
+				<h1 class="text-xl font-semibold text-gray-800 dark:text-gray-300">
+					{'Name Here'}
+				</h1>
+				<Badge class="h-4 text-xs text-gray-600 dark:text-gray-400" color="dark" border>
+					<ClockSolid class="w-2.5 h-2.5 me-1.5" />
+					22/22/2022
+				</Badge>
 			</div>
 		</div>
+
+		<!-- post image's -->
+		<div class="">
+			<img class="object-cover object-center w-full h-64 rounded-lg" src={Food} alt="" />
+		</div>
+
+		<div class="mt-5">
+			<h1 class="mt-4 text-xl font-bold text-gray-800 dark:text-white">Free meals</h1>
+
+			<p class="mt-2 text-gray-500 dark:text-gray-400">
+				I have 10 meal vouchers for a local restaurant. DM me if you need it.
+			</p>
+
+			<div class="flex justify-between py-4">
+				<p class="text-lg pt-2 font-meduim text-gray-500">3 hourse ago</p>
+				<button
+					class="p-2 px-3 rounded-xl bg-blue-400 text-white font-semibold"
+					on:click={() => (progress = `${Math.round(Math.random() * 100)}`)}>
+					Claim Offer
+				</button>
+			</div>
+		</div>
+		<Progressbar animate {progress} color="gray" />
 	</div>
 </section>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,15 +3,33 @@ import { myCustomTheme } from './freecaret-theme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import { skeleton } from '@skeletonlabs/tw-plugin';
+
+import flowbite from 'flowbite/plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: 'class',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
 		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
-	],
+	,
+	'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {      colors: {
+			// flowbite-svelte
+			primary: {
+			  50: '#FFF5F2',
+			  100: '#FFF1EE',
+			  200: '#FFE4DE',
+			  300: '#FFD5CC',
+			  400: '#FFBCAD',
+			  500: '#FE795D',
+			  600: '#EF562F',
+			  700: '#EB4F27',
+			  800: '#CC4522',
+			  900: '#A5371B'
+			}
+		  }}
 	},
 	plugins: [
 		forms,
@@ -31,5 +49,6 @@ export default {
 				custom: [myCustomTheme]
 			}
 		})
-	]
+	,
+	flowbite]
 };
