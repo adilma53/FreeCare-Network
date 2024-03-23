32e502f265a8436f2e2d4c31be79c62af4663304
div-styl <gouhmada@gmail.com>
2024-03-23 00:41:37 +0000

    rounder issue

--- frontend-nextjs/src/app/components/Body/mobile_body.jsx
+++ frontend-nextjs/src/app/components/Body/mobile_body.jsx
@@ -5,27 +5,29 @@ import React from "react";
 import { Card, CardHeader, CardBody, CardFooter, Button } from "@nextui-org/react";
 export function BodyMobile() {
   return (
-    <div className="flex items-center justify-center p-3">
-      <Card
-          isPressable onPress={() => console.log("item pressed")}  
+    <>
+      <div className="flex items-center justify-center p-3">
+        <Card
+          isPressable onPress={() => console.log("item pressed")}
           className="px-auto py-auto">
-        <CardBody className="space-y-5">
-          <Image src={food} alt="pic of food" className="object-cover object-center w-full h-64 overflow-visible rounded-lg" />
-          <div>
-            <h1 className="mt-4 text-xl font-bold text-gray-800 dark:text-white">Free Meals</h1>
-            <p className="mt-2 text-gray-500 dark:text-gray-400">
-              We offer free meals here in the shop for Ramadan
-              Your are welcomed anyway 😊
-            </p>
-          </div>
-        </CardBody>
-        <CardFooter className="p-2 flex justify-between py-5">
-          <p className="text-lg pt-2 font-meduim text-gray-500">3 hourse ago</p>
-          <Button color="primary" variant="flat" radius="small">
-            Claim Offer
-          </Button>
-        </CardFooter>
-      </Card>
-    </div>
+          <CardBody className="space-y-5">
+            <div>
+              <Image src={food} alt="pic of food" className="object-cover object-center w-full h-64 rounded-xl" />
+              <h1 className="mt-4 text-xl font-bold text-gray-800 dark:text-white">Iftar In Ramadan</h1>
+              <p className="mt-2 text-gray-500 dark:text-gray-400">
+                We offer free meals here in the shop for Ramadan
+                Your are welcomed anyway 😊
+              </p>
+            </div>
+          </CardBody>
+          <CardFooter className="p-2 flex justify-between py-5">
+            <p className="text-lg pt-2 font-meduim text-gray-500">3 hourse ago</p>
+            <Button color="primary" variant="flat" radius="small">
+              Claim Offer
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </>
   );
 }
