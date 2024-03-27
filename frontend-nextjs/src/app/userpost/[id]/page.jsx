5bf1330a6ee3da9329c8ebb7f107a4f5b5566ba8
div-styl <gouhmada@gmail.com>
2024-03-27 01:42:53 +0000

    Add responsive behavior to UserPost component

--- frontend-nextjs/src/app/userpost/[id]/page.jsx
+++ frontend-nextjs/src/app/userpost/[id]/page.jsx
@@ -3,7 +3,9 @@ import { InsidePost } from "@/components/Body/insidepost";
 export default function UserPost({ params }) {
   return (
     <>
-      <InsidePost params={params} />
+      <div className="md:hidden">
+        <InsidePost params={params} />
+      </div>
     </>
   );
 }
